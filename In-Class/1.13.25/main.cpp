226db31c6444025bf4d4ddf1e87a706ac1cf6bc2
ethan-leonard <ethan.leonard821@gmail.com>
2025-01-13 13:33:23 -0800

    update 1.13.25

---
+++ In-Class/1.13.25/main.cpp
@@ -0,0 +1,27 @@
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+// 101. Symmetric Tree
+class Solution {
+public:
+    bool isSymmetric(TreeNode* p, TreeNode* q) {
+        if (p == nullptr && q == nullptr) return true;
+        if (p == nullptr || q == nullptr) return false;
+        if (p->val != q->val) return false;
+
+        return isSymmetric(p->left, q->right) && isSymmetric(p->right, q->left);
+    }
+
+    bool isSymmetric(TreeNode* root) {
+        if (root == nullptr) {
+            return true;
+        } return isSymmetric(root->left, root->right);
+    }
+};
