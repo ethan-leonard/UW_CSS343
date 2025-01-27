f808a683640c0475078ccf2e9330867753acfd3e
ethan-leonard <ethan.leonard821@gmail.com>
2025-01-27 14:46:41 -0800

    update 1.27.25

--- In-Class/1.27.25/main.cpp
+++ In-Class/1.27.25/main.cpp
@@ -17,3 +17,16 @@ public:
         } return root;
     }
 };
+
+// 104. Maximum Depth of Binary Tree
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (root == nullptr) return 0;
+
+        int lHeight = maxDepth(root->left);
+        int rHeight = maxDepth(root->right);
+
+        return max(lHeight, rHeight) + 1;
+    }
+};
